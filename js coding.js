f820952179b0ef5f4aaf4add6ecbaab39b837364
Ayman Tamim <aymantamim10409@gmail.com>
2026-05-10 17:20:43 +0600

    Add files via upload
    please try this.

---
+++ js coding.js
@@ -0,0 +1,36 @@
+var input = document.querySelector(".inc");
+var buttons = document.querySelectorAll("input[type='button']");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        var value = this.value;
+
+        if (value === "C") {
+            input.value = "";
+        }
+
+        else if (value === "DEL") {
+            input.value = input.value.slice(0, -1);
+        }
+
+        else if (value === "=") {
+            try {
+                input.value = eval(input.value);
+            } catch {
+                input.value = "Error";
+            }
+        }
+
+        else if (value === "+/-") {
+            input.value = -input.value;
+        }
+
+        else if (value === "%") {
+            input.value = input.value / 100;
+        }
+
+        else {
+            input.value += value;
+        }
+    });
+}
